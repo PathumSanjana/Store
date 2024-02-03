de55d821495aab4f34a86b4c95c1c8b0f6ae97e4
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2024-02-03 14:43:35 +0530

    students data get to an array

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student_name',
+        'student_email',
+        'student_address',
+        'student_dob',   
+    ];
 }
